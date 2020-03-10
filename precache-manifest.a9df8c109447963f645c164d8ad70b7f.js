7832df0d5d76d42af7880f697911962debced842
tomsza26 <tomsza27@gmail.com>
2020-03-10 22:25:48 +0100

    Updates

---
+++ precache-manifest.a9df8c109447963f645c164d8ad70b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b98c4dd4a6d15839cbd1d7132ddc751",
+    "url": "/markdown-notes/index.html"
+  },
+  {
+    "revision": "c564a66a526a95be3b02",
+    "url": "/markdown-notes/static/css/main.6f0a74b7.chunk.css"
+  },
+  {
+    "revision": "eea0b088df57b4a223d9",
+    "url": "/markdown-notes/static/js/2.5af7073e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/markdown-notes/static/js/2.5af7073e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c564a66a526a95be3b02",
+    "url": "/markdown-notes/static/js/main.8bc17926.chunk.js"
+  },
+  {
+    "revision": "0b0df5f9ef70a0ea9570",
+    "url": "/markdown-notes/static/js/runtime-main.aa4fbfe0.js"
+  }
+]);
